085569605588f197565a7c88852d547472e99bd2
Jatin Kumar <73215164+jatinkumar24@users.noreply.github.com>
2024-09-29 08:01:29 +0000

    2

---
+++ js/conversionOperation.js
@@ -0,0 +1,30 @@
+let score ="24";
+
+console.log (typeof score);
+
+
+let ValueInNumber = Number(score);
+console.log (typeof ValueInNumber);
+
+let code ="2245adc";
+
+console.log(typeof code);
+
+let ValueInNumber2 = Number(code);
+console.log(typeof ValueInNumber2);
+console.log(ValueInNumber2);    //NaN means Not a Number , So don't rely Blindly on "Number" 
+
+let ID = 26;
+console.log(typeof ID);
+ 
+let ValueInString = String(ID);
+console.log(typeof ValueInString);
+console.log(ValueInString);
+
+
+
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("practising Javascript");
+
+let Name = "jatin";
+const RollNo =12;
+var Class = 10
+Age = 24;
+
+
+console.table([Name, RollNo, Class,Age]);
