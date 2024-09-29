cb1341d9ab28ee7649521fc9435c92eb58eef665
Jatin Kumar <73215164+jatinkumar24@users.noreply.github.com>
2024-09-29 11:51:39 +0000

    3

---
+++ js/operations.js
@@ -0,0 +1,27 @@
+let Value = 56;
+// console.log(Value);
+
+let NegValue = -Value;
+// console.log(NegValue);
+
+// console.log(4+8);
+// console.log(4-8);
+// console.log(4*8);
+// console.log(4**2);
+// console.log(4/8);
+
+let str1 = "Hello ";
+let str2 = "Jatin";
+let str3 = str1+str2;
+// console.log(str3);
+console.log(3+3);
+console.log(3+"3");
+console.log("3"+3);
+console.log("3"+2+5); //if String is first it will assume all char as String
+console.log(2+5+"3");  //if String is last than it will perform operations after that it will add string to the result 
+
+
+
+
+
+
