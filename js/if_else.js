1b02b518217f66c1ab8597ecaa1ef0654c7ad21f
Jatin Kumar <73215164+jatinkumar24@users.noreply.github.com>
2024-10-04 12:01:29 +0530

    if else

---
+++ js/if_else.js
@@ -0,0 +1,169 @@
+// const one = 2
+// const two = 5
+// const three = 2
+
+// ++++++++++++++++++++++++++++++++++     if else statement        ++++++++++++++++++++++++++++++++++++++++++++++
+//  if (condition) {
+//     statement 1
+//  }
+
+// if (one==three) {
+//     console.log("vlaue is true");
+    
+// }
+
+// if (one==three) {
+//     console.log("vlaue is true");
+// }    
+// console.log("vlaue is  not true");
+// in this is condition is true than both values are been printed to avoid this we use else key after if statement scope
+
+// 
+
+
+
+
+//    console.log("Execute");
+// <, >, <=, >=, ==, !={not equal}, ==={equal and datatype is same}, !=={is checks not equal and datatype also}
+
+// if (2!=="2") {
+//     console.log('2 is not equal to "2"');
+    
+// } else
+//       console.log('2 is equal to "2"');
+      
+
+// ++++++++++++++++++++if else nested loop example++++++++++++++++++++++++++++++   
+
+
+// let marks = 10;
+
+// if (marks<33) {
+//     console.log(`obtained marks is ${marks}. which is below passing marks`);
+// }
+// else if (marks>=33 && marks<=60) {
+//     console.log("pass with Grade D ");
+    
+// } else if (marks>60 && marks<=75) {
+//     console.log("pass with Grade C ");
+    
+// } else if (marks>75 && marks<=85) {
+//     console.log("pass with Grade B ");
+    
+// } else if (marks>85 && marks<=95) {
+//     console.log("pass with Grade A ");
+    
+// }else console.log("pass with Grade S ");
+
+
+// function testNum(a) {
+//     let result;
+//     if (a > 0) {
+//       result = 'positive';
+//     } else {
+//       result = 'NOT positive';
+//     }
+//     return result;
+//   }
+  
+//   console.log(testNum(-5));
+
+// function score(points) {
+//  let Grade;
+//  if (points>300 && points<=500) {
+//     Grade= "B Grade";
+//  }   else if (points>500 && points<=850) {
+//     Grade = "A Grade";
+//  } else 
+//        Grade = "S Grade";
+//     return Grade;
+// }
+
+// console.log(score (900));
+
+
+let Aadhar= true;
+let PANcard = true;
+//  condition via OR statement
+if (Aadhar || PANcard || 2==4) {
+     console.log("varification is possible");
+     
+} else console.log("varificaton is not possible");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
