9640588d30ce5ae522ac66c3c9acea6fa549fedb
Jatin Kumar <73215164+jatinkumar24@users.noreply.github.com>
2024-10-04 12:11:04 +0530

    switch

---
+++ js/switch.js
@@ -0,0 +1,28 @@
+const Day = "Sunday";
+
+switch (Day) {
+    case "Monday":
+        console.log("its Monday");
+        break;
+    case "Tuseday":
+        console.log("its Tuseday");
+        break;
+    case "Wednsday":
+        console.log("its Wednsday");
+        break;
+    case "Thursday":
+        console.log("its Thursday");
+        break;
+    case "Friday":
+        console.log("its Friday");
+        break;
+    case "Saturday":
+        console.log("its Saturday");
+        break;
+    case "Sunday":
+        console.log("its Sunday");
+        break;
+    default: console.log("Please enter a valid Day");
+    
+     break;
+}
